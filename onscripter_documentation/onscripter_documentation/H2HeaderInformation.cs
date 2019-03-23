b77a981b6165a6fdf26b00b5056c498777b6c4f2
Daniel Wong <drojfjord@gmail.com>
2019-03-23 16:17:19 +1100

    Refactor into separate classes

---
+++ onscripter_documentation/onscripter_documentation/H2HeaderInformation.cs
@@ -0,0 +1,69 @@
+﻿using AngleSharp.Dom;
+
+namespace onscripter_documentation
+{
+    class H2HeaderInformation
+    {
+        public readonly string sectionsWhereCommandCanBeUsed;
+        public readonly string versionsWhereCommandCanBeUsed;
+        public readonly string wordName;
+
+        public H2HeaderInformation(string sectionsWhereCommandCanBeUsed, string versionsWhereCommandCanBeUsed, string wordName)
+        {
+            this.sectionsWhereCommandCanBeUsed = sectionsWhereCommandCanBeUsed;
+            this.versionsWhereCommandCanBeUsed = versionsWhereCommandCanBeUsed;
+            this.wordName = wordName;
+        }
+
+        public override string ToString()
+        {
+            return $"[{sectionsWhereCommandCanBeUsed}] ({versionsWhereCommandCanBeUsed}): {wordName}";
+        }
+
+        //Example of H2 Header Block:
+        //
+        // <div class="Support">
+        //     <span class="WordVersion">
+        //         Ver.(undoc)
+        //     </span>
+        //     <span class="WordField">
+        //         <span class="NoBr">
+        //             [Definition/Program Block]
+        //         </span>
+        //     </span>
+        //     <span class="EngineField">
+        //         <span class="NoBr">
+        //             ( NScr
+        //             <span class="EngineVersion">
+        //                 2.03
+        //             </span>
+        //         </span>
+        //         ,
+        //         <span class="NoBr">
+        //             ONScr-EN
+        //             <span class="EngineVersion">
+        //                 20091010
+        //             </span>
+        //             )
+        //         </span>
+        //     </span>
+        // </div>
+        // <div class="WordName">
+        //     humanpos
+        // </div>
+        public static H2HeaderInformation ParseH2HeaderBlock(IElement h2HeaderElement)
+        {
+            string asRawText = Util.ConvertToTextStringWithoutWhitespace(Util.GetFirstElementOfClassOrError(h2HeaderElement, "Support"));
+
+            string[] splitHeader = asRawText.Split(new char[] { '[', ']', '(', ')' });
+            string sectionsWhereCommandCanBeUsed = splitHeader[1].Trim();
+            string versionsWhereCommandCanBeUsed = splitHeader[3].Trim();
+            Util.ExceptionIfStringEmpty(sectionsWhereCommandCanBeUsed);
+            Util.ExceptionIfStringEmpty(versionsWhereCommandCanBeUsed);
+
+            string wordName = Util.ConvertToTextStringWithoutWhitespace(Util.GetFirstElementOfClassOrError(h2HeaderElement, "WordName"));
+
+            return new H2HeaderInformation(sectionsWhereCommandCanBeUsed, versionsWhereCommandCanBeUsed, wordName);
+        }
+    }
+}
---
+++ onscripter_documentation/onscripter_documentation/Util.cs
@@ -0,0 +1,55 @@
+﻿using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace onscripter_documentation
+{
+    class Util
+    {
+        static Regex whitespaceRegex = new Regex(@"\s+", RegexOptions.Compiled);
+        static Regex H2HeaderRegex = new Regex(@"\[([^]]*)]\s*\(([^\)]*)\)", RegexOptions.Compiled);
+
+        public static void ExceptionIfStringEmpty(string s)
+        {
+            if (s == String.Empty)
+            {
+                throw new Exception("String is Empty!");
+            }
+        }
+
+        public static void ExceptionIfListCountNotOne<T>(List<T> l)
+        {
+            if (l.Count != 1)
+            {
+                throw new Exception("List not of size 1");
+            }
+        }
+
+        public static IElement GetFirstElementOfClassOrError(IElement el, string className)
+        {
+            var childrenWithClassName = el.Children.Where((c) => c.ClassName == className).ToList();
+            if (childrenWithClassName.Count != 1)
+            {
+                throw new Exception("List not of size 1");
+            }
+            return childrenWithClassName.First();
+        }
+
+        public static IElement GetFirstElementOrError(IElement el)
+        {
+            if (el.ChildElementCount != 1)
+            {
+                throw new Exception("List not of size 1");
+            }
+
+            return el.FirstElementChild;
+        }
+
+        public static string ConvertToTextStringWithoutWhitespace(INode el)
+        {
+            return whitespaceRegex.Replace(el.Text(), " ").Trim();
+        }
+    }
+}
