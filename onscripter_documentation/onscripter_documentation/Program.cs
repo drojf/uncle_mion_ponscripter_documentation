0c4620ae0e6cf501743487e150351005cb8a117c
Daniel Wong <drojfjord@gmail.com>
2019-03-24 23:44:53 +1100

    begin adding output of html files.

--- onscripter_documentation/onscripter_documentation/Program.cs
+++ onscripter_documentation/onscripter_documentation/Program.cs
@@ -7,13 +7,61 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Linq;
 using Newtonsoft.Json;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html;
 
 namespace onscripter_documentation
 {
     class Program
     {
+        static IElement GenerateElementFromFunctionEntry(IHtmlDocument doc, FunctionEntry fe)
+        {
+            IElement functionEntryRoot = doc.CreateElement("div");
+            functionEntryRoot.ClassName = "FunctionEntry";
+
+            //Create function link
+            var idLink = doc.CreateElement("a");
+            idLink.Id = fe.id;
+            functionEntryRoot.AppendChild(idLink);
+
+            //Create function name
+            var functionName = doc.CreateElement("h2");
+            functionName.TextContent = fe.headerInformation.wordName;
+            functionEntryRoot.AppendChild(functionName);
+
+            //Create function description
+            var functionDescription = doc.CreateElement("div");
+            functionDescription.ClassName = "FunctionDescription";
+            functionDescription.InnerHtml = fe.functionDescription.descriptionHTML;
+            functionEntryRoot.AppendChild(functionDescription);
+
+            return functionEntryRoot;
+        }
+
+        static string GenerateDocument(string templateFilePath, List<FunctionEntry> functionEntries)
+        {
+            var document = new HtmlParser().ParseDocument(File.ReadAllText(templateFilePath, Encoding.UTF8));
+
+            // Insert generated document header information here
+            var p = document.CreateElement("p");
+            p.TextContent = "Insert generated document header information here";
+            document.Body.AppendChild(p);
+
+            foreach (FunctionEntry functionEntry in functionEntries)
+            {
+                document.Body.AppendChild(GenerateElementFromFunctionEntry(document, functionEntry));
+            }
+
+            var sw = new StringWriter();
+            document.ToHtml(sw, new PrettyMarkupFormatter());
+            return sw.ToString();
+        }
+
         static void Main(string[] args)
         {
+            string templateFilePath = "template.html";
+
             string htmlPath = @"..\..\..\..\WaybackArchive\NScripter API Reference [compiled by senzogawa, translated_annotated_XML-ized by Mion (incorporates former translation by Seung 'gp32' Park)].html";
 
             //read in from html file
@@ -45,6 +93,10 @@ namespace onscripter_documentation
                     functionEntries.Add(ent);
                 }
             }
+
+            string output = GenerateDocument(templateFilePath, functionEntries);
+            Console.WriteLine(output);
+            File.WriteAllText("GeneratedDocumentation.html", output);
 
             //write out function entries as json
             JsonSerializer serializer = new JsonSerializer();
